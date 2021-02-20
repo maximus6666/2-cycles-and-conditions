fcf09267a84c97f1ef3c7c53ef4cb32f9510d856
Maksym Dmytryshyn <dmytryshyn_max@ukr.net>
2021-02-20 21:49:22 +0200

    Add cycle and integer check

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('test');
+let startNumberN = '';
+let finishNumberM = '';
+let totalSum = 0;
+//запускаємо питання
+do {
+  startNumberN = prompt('Введіть число N');
+} while (!parseInt(startNumberN));
+
+do {
+  finishNumberM = prompt('Введіть число M');
+} while (!parseInt(finishNumberM));
+
+// запитуємось чи пропускати парні
+const passEven = confirm('Пропускати парні?');
+
+// цикл
+if (+startNumberN < +finishNumberM) {
+  for (let i = +startNumberN; i <= +finishNumberM; i++) {
+    if (i % 2 === 0 && passEven) {
+      continue;
+    }
+    totalSum += i;
+  }
+  console.log(`Сума чисел з ${startNumberN} по ${finishNumberM}: ${totalSum}`);
+} else {
+  console.log('введіть число N менше за число М!');
+}
