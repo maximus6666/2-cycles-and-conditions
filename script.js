3cf467bc8642e3bc478dddd166319ab187779a53
Maksym Dmytryshyn <dmytryshyn_max@ukr.net>
2021-02-21 17:24:57 +0200

    Convert a string value to a number

--- script.js
+++ script.js
@@ -4,24 +4,28 @@ let totalSum = 0;
 //запускаємо питання
 do {
   startNumberN = prompt('Введіть число N');
-} while (!parseInt(startNumberN));
+} while ( !parseInt(startNumberN));
 
 do {
   finishNumberM = prompt('Введіть число M');
 } while (!parseInt(finishNumberM));
 
+let parseStartNumberN = parseInt(startNumberN);
+let parseFinishNumberM = parseInt(finishNumberM);
+
+
 // запитуємось чи пропускати парні
 const passEven = confirm('Пропускати парні?');
 
 // цикл
-if (+startNumberN < +finishNumberM) {
-  for (let i = +startNumberN; i <= +finishNumberM; i++) {
+if (parseStartNumberN < parseFinishNumberM) {
+  for (let i = parseStartNumberN; i <= parseFinishNumberM; i++) {
     if (i % 2 === 0 && passEven) {
       continue;
     }
     totalSum += i;
   }
-  console.log(`Сума чисел з ${startNumberN} по ${finishNumberM}: ${totalSum}`);
+  console.log(`Сума чисел з ${parseStartNumberN} по ${parseFinishNumberM}: ${totalSum}`);
 } else {
   console.log('введіть число N менше за число М!');
 }
